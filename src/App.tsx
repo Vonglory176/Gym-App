e7597512f92f56fb249160a6481ba3c2af6c0a08
Vonglory176 <SkylerGConley@gmail.com>
2024-08-11 17:25:10 -0700

    Added code for workout state

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,41 @@ import './index.css'
 import Hero from './components/Hero'
 import Generator from './components/Generator'
 import Workout from './components/Workout'
+import { generateWorkout } from './utils/functions'
 
 function App() {
+  const [currentWorkout, setCurrentWorkout] = useState<string>("")
+  const [poison, setPoison] = useState<string>("individual")
+  const [muscles, setMuscles] = useState<string[]>([])
+  const [goal, setGoal] = useState<string>("strength_power")
+
+  const updateWorkout = () => {
+    console.log("updateWorkout")
+
+    if (muscles.length < 1) return
+
+    const newWorkout = generateWorkout({poison, muscles, goal})
+    console.log(newWorkout)
+
+    setCurrentWorkout(newWorkout)
+    window.location.href = '#workout'
+  }
 
   return (
     <main className='min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white text-sm sm:text-base'>
       <Hero />
-      <Generator />
-      <Workout />
+
+      <Generator 
+        poison={poison}
+        setPoison={setPoison}
+        muscles={muscles}
+        setMuscles={setMuscles}
+        goal={goal}
+        setGoal={setGoal}
+        updateWorkout={updateWorkout}
+      /> {/* children="" */}
+
+      {currentWorkout && <Workout workout={currentWorkout} />}
     </main>
   )
 }
