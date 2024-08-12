bd9d06897e3a4004e4622b0c2e6c4cd2ce7d2e5d
Vonglory176 <SkylerGConley@gmail.com>
2024-08-11 17:31:14 -0700

    New file for displaying a full workout plan

--- src/components/Workout.tsx
+++ src/components/Workout.tsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import SectionWrapper from './SectionWrapper'
+import ExerciseCard from './ExerciseCard'
+import { WorkoutProps, ExerciseProps } from '../utils/types'
 
-const Workout = () => {
+// interface ExerciseProps {
+//   name: string
+//   type: string
+// }
+
+// interface WorkoutProps {
+//   workout: ExerciseProps[]
+//   // exercise: object[]
+// }
+
+const Workout: React.FC<WorkoutProps> = ({workout}) => {
   return (
-    <div>
-      
-    </div>
+
+    <SectionWrapper id='workout' header={"Welcome to"} title={['The', 'DANGER', 'zone']}>
+      <div className='flex flex-col gap-4'>
+        {workout.map((exercise: ExerciseProps, i: number) => {
+          return (
+            <ExerciseCard exercise={exercise} index={i} key={i} />
+          )
+        })}
+      </div>
+    </SectionWrapper>
   )
 }
 
