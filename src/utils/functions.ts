ea9406b5a80b689ee4a1dc25267364669b3f92f3
Vonglory176 <SkylerGConley@gmail.com>
2024-08-12 12:58:51 -0700

    Converted and updated to use TS format

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,87 +1,90 @@
 import { EXERCISES, SCHEMES, TEMPOS, WORKOUTS } from "./swoldier"
+
+interface GenerateWorkoutArgs {
+    muscles: string[]
+    poison: string
+    goal: string
+}
+
 const exercises = exercisesFlattener(EXERCISES)
 
-// export function generateWorkout({muscles, poison, goal}: {muscles: string[], poison: string, goal: string}) {
-export function generateWorkout(args) {
-    const { muscles, poison: workout, goal } = args
-    let exer = Object.keys(exercises);
-    exer = exer.filter((key) => exercises[key].meta.environment !== "home");
-    let includedTracker = [];
-    let numSets = 5;
-    let listOfMuscles;
+export function generateWorkout({ muscles, poison: workout, goal }: GenerateWorkoutArgs) {
+    let exer = Object.keys(exercises)
+    exer = exer.filter((key) => exercises[key].meta.environment !== "home")
+    let includedTracker: string[] = []
+    // let numSets = 5
+    let listOfMuscles: string[]
 
     if (workout === "individual") {
-        listOfMuscles = muscles;
+        listOfMuscles = muscles
     } else {
-        listOfMuscles = WORKOUTS[workout][muscles[0]];
+        listOfMuscles = (WORKOUTS[workout as keyof typeof WORKOUTS] as Record<string, string[]>)[muscles[0]]
     }
 
-    listOfMuscles = new Set(shuffleArray(listOfMuscles));
-    let arrOfMuscles = Array.from(listOfMuscles);
+    listOfMuscles = Array.from(new Set(shuffleArray(listOfMuscles)))
+    let arrOfMuscles = Array.from(listOfMuscles)
     let scheme = goal
-    let sets = SCHEMES[scheme].ratio
-        .reduce((acc, curr, index) => {
-            //make this compound and exercise muscle -> array of objects and destructure in loop
+    let sets = SCHEMES[scheme as keyof typeof SCHEMES].ratio
+        .reduce<string[]>((acc, curr, index) => {
             return [
                 ...acc,
-                ...[...Array(parseInt(curr)).keys()].map((val) =>
+                ...[...Array(Number(curr)).keys()].map(() =>
                     index === 0 ? "compound" : "accessory"
                 ),
-            ];
+            ]
         }, [])
-        .reduce((acc, curr, index) => {
+        .reduce<{ setType: string, muscleGroup: string }[]>((acc, curr, index) => {
             const muscleGroupToUse =
                 index < arrOfMuscles.length
                     ? arrOfMuscles[index]
-                    : arrOfMuscles[index % arrOfMuscles.length];
+                    : arrOfMuscles[index % arrOfMuscles.length]
             return [
                 ...acc,
                 {
                     setType: curr,
                     muscleGroup: muscleGroupToUse,
                 },
-            ];
-        }, []);
+            ]
+        }, [])
 
     const { compound: compoundExercises, accessory: accessoryExercises } =
-        exer.reduce(
+        exer.reduce<{ compound: Record<string, any>, accessory: Record<string, any> }>(
             (acc, curr) => {
-                let exerciseHasRequiredMuscle = false;
+                let exerciseHasRequiredMuscle = false
                 for (const musc of exercises[curr].muscles) {
-                    if (listOfMuscles.has(musc)) {
-                        exerciseHasRequiredMuscle = true;
+                    if (listOfMuscles.includes(musc)) {
+                        exerciseHasRequiredMuscle = true
                     }
                 }
                 return exerciseHasRequiredMuscle
                     ? {
                         ...acc,
                         [exercises[curr].type]: {
-                            ...acc[exercises[curr].type],
+                            ...acc[exercises[curr].type as keyof typeof acc],
                             [curr]: exercises[curr],
                         },
                     }
-                    : acc;
+                    : acc
             },
             { compound: {}, accessory: {} }
-        );
+        )
 
     const genWOD = sets.map(({ setType, muscleGroup }) => {
         const data =
-            setType === "compound" ? compoundExercises : accessoryExercises;
-        const filteredObj = Object.keys(data).reduce((acc, curr) => {
+            setType === "compound" ? compoundExercises : accessoryExercises
+        const filteredObj = Object.keys(data).reduce<Record<string, any>>((acc, curr) => {
             if (
                 includedTracker.includes(curr) ||
                 !data[curr].muscles.includes(muscleGroup)
             ) {
-                // if (includedTracker.includes(curr)) { console.log('banana', curr) }
-                return acc;
+                return acc
             }
-            return { ...acc, [curr]: exercises[curr] };
-        }, {});
-        const filteredDataList = Object.keys(filteredObj);
+            return { ...acc, [curr]: exercises[curr] }
+        }, {})
+        const filteredDataList = Object.keys(filteredObj)
         const filteredOppList = Object.keys(
             setType === "compound" ? accessoryExercises : compoundExercises
-        ).filter((val) => !includedTracker.includes(val));
+        ).filter((val) => !includedTracker.includes(val))
 
         let randomExercise =
             filteredDataList[
@@ -89,54 +92,51 @@ export function generateWorkout(args) {
             ] ||
             filteredOppList[
             Math.floor(Math.random() * filteredOppList.length)
-            ];
-
-        // console.log(randomExercise)
+            ]
 
         if (!randomExercise) {
-            return {};
+            return {}
         }
 
         let repsOrDuraction =
             exercises[randomExercise].unit === "reps"
-                ? Math.min(...SCHEMES[scheme].repRanges) +
+                ? Math.min(...SCHEMES[scheme as keyof typeof SCHEMES].repRanges) +
                 Math.floor(
                     Math.random() *
-                    (Math.max(...SCHEMES[scheme].repRanges) -
-                        Math.min(...SCHEMES[scheme].repRanges))
+                    (Math.max(...SCHEMES[scheme as keyof typeof SCHEMES].repRanges) -
+                        Math.min(...SCHEMES[scheme as keyof typeof SCHEMES].repRanges))
                 ) +
                 (setType === "accessory" ? 4 : 0)
-                : Math.floor(Math.random() * 40) + 20;
-        const tempo = TEMPOS[Math.floor(Math.random() * TEMPOS.length)];
+                : Math.floor(Math.random() * 40) + 20
+        const tempo = TEMPOS[Math.floor(Math.random() * TEMPOS.length)]
 
         if (exercises[randomExercise].unit === "reps") {
             const tempoSum = tempo
                 .split(" ")
-                .reduce((acc, curr) => acc + parseInt(curr), 0);
-            if (tempoSum * parseInt(repsOrDuraction) > 85) {
-                repsOrDuraction = Math.floor(85 / tempoSum);
+                .reduce((acc, curr) => acc + parseInt(curr), 0)
+            if (tempoSum * parseInt(repsOrDuraction.toString()) > 85) {
+                repsOrDuraction = Math.floor(85 / tempoSum)
             }
         } else {
-            //set to nearest 5 seconds
-            repsOrDuraction = Math.ceil(parseInt(repsOrDuraction) / 5) * 5;
+            repsOrDuraction = Math.ceil(parseInt(repsOrDuraction.toString()) / 5) * 5
         }
-        includedTracker.push(randomExercise);
+        includedTracker.push(randomExercise)
 
         return {
             name: randomExercise,
             tempo,
-            rest: SCHEMES[scheme]["rest"][setType === "compound" ? 0 : 1],
+            rest: SCHEMES[scheme as keyof typeof SCHEMES]["rest"][setType === "compound" ? 0 : 1],
             reps: repsOrDuraction,
             ...exercises[randomExercise],
-        };
-    });
+        }
+    })
 
     return genWOD.filter(
         (element) => Object.keys(element).length > 0
-    );
+    )
 }
 
-function shuffleArray(array) {
+function shuffleArray(array: any[]): any[] {
     for (let i = array.length - 1; i > 0; i--) {
         let j = Math.floor(Math.random() * (i + 1))
         let temp = array[i]
@@ -146,8 +146,8 @@ function shuffleArray(array) {
     return array
 }
 
-function exercisesFlattener(exercisesObj) {
-    const flattenedObj = {}
+function exercisesFlattener(exercisesObj: Record<string, any>): Record<string, any> {
+    const flattenedObj: Record<string, any> = {}
 
     for (const [key, val] of Object.entries(exercisesObj)) {
         if (!("variants" in val)) {
@@ -157,7 +157,7 @@ function exercisesFlattener(exercisesObj) {
                 let variantName = variant + "_" + key
                 let variantSubstitutes = Object.keys(val.variants).map((element) => {
                     return element + ' ' + key
-                }).filter(element => element.replaceAll(' ', '_') !== variantName)
+                }).filter(element => element.replace(/ /g, '_') !== variantName)
 
                 flattenedObj[variantName] = {
                     ...val,
