db23bf226a102009e8794fe88aba565404a274b9
Vonglory176 <SkylerGConley@gmail.com>
2024-08-12 12:59:41 -0700

    Added extra padding to the bottom of the section

--- src/components/SectionWrapper.tsx
+++ src/components/SectionWrapper.tsx
@@ -11,7 +11,7 @@ import { SectionWrapperProps } from '../utils/types'
 const SectionWrapper: React.FC<SectionWrapperProps> = ({ children, header, title, id }) => {
 
   return (
-    <section className='min-h-screen flex flex-col gap-10' id={id}>
+    <section className='min-h-screen flex flex-col gap-10 pb-10' id={id}>
       <div className="bg-slate-950 py-10 flex flex-col gap-2 justify-center items-center p-4">
         <p className='uppercase font-medium'>{header}</p>
         <h2 className='font-semibold text-3xl sm:text-4xl md:text-5xl lg:text-6xl'>{title[0]} <span className='uppercase text-blue-400'>{title[1]}</span> {title[2]}</h2>
